ec1d752ab50f362b550da9bd32d8f2eee12f37b4
Aidan Simard <aidansimard01@gmail.com>
2023-02-05 11:09:26 -0500

    Test adding static files url pattern

--- team_management_app/urls.py
+++ team_management_app/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('team/', include('team_manager.urls')),
-    path('admin/', admin.site.urls),
+    path("team/", include("team_manager.urls")),
+    path("admin/", admin.site.urls),
+    staticfiles_urlpatterns(),
 ]
