77d82fa4cd543a78bfd3fd052800953f23649918
Aidan Simard <aidansimard01@gmail.com>
2023-02-05 11:29:23 -0500

    test adding static url

--- team_management_app/urls.py
+++ team_management_app/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("team/", include("team_manager.urls")),
     path("admin/", admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
